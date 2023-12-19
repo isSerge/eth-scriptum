16e3e5141533f92aec7fa33e455ac326f4feebf5
Serge Kovbasiuk <isserge@protonmail.com>
2023-12-19 17:52:57 +0700

    Add TODO comments and update UI in TextInscription component

--- src/TextInscription.tsx
+++ src/TextInscription.tsx
@@ -11,6 +11,7 @@ import { CopyIcon } from './Icons';
 const prefix = 'data:,';
 
 // TODO: check availability before inscribing
+// TODO: change success links for other chains (not only mainnet)
 export function TextInscription() {
   const [text, setText] = useState('');
   const [encodedText, setEncodedText] = useState(prefix);
@@ -46,7 +47,7 @@ export function TextInscription() {
     }, 250);
   };
 
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
     const text = e.target.value;
     setText(text);
     setEncodedText(`data:,${text}`);
@@ -55,26 +56,51 @@ export function TextInscription() {
 
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 p-4 max-w-lg">
-      <input
+      <textarea
         autoFocus
-        className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+        className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 resize-none"
         name="text"
-        placeholder="Text to ethscribe"
+        placeholder="Text to inscribe"
         onChange={handleChange}
         value={text}
-      />
-      <div className="mb-4 p-2 bg-white rounded-md shadow w-full">
-        {encodedText}
+        rows={4}
+        style={{ minHeight: '100px' }}
+      ></textarea>
+
+      <div className="mb-4 bg-white rounded-md shadow w-full">
+        <div className="p-4 border-b border-gray-200">
+          <label className="text-sm font-medium text-gray-700 mb-2">
+            Encoded Text
+          </label>
+          <div
+            className="mt-1 text-gray-800 max-h-32 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400"
+            style={{ wordBreak: 'break-all' }}
+          >
+            {encodedText}
+          </div>
+        </div>
+
+        <div className="p-4 border-b border-gray-200">
+          <div className="flex items-center justify-between mb-2">
+            <label className="text-sm font-medium text-gray-700">
+              Hex Value
+            </label>
+            <button
+              onClick={handleCopyClick}
+              className="rounded hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            >
+              <CopyIcon />
+            </button>
+          </div>
+          <div
+            className="text-gray-800 flex-grow max-h-32 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400"
+            style={{ wordBreak: 'break-all' }}
+          >
+            {hex}
+          </div>
+        </div>
       </div>
-      <div className="flex items-center mb-4 p-2 bg-white rounded-md shadow w-full">
-        <div className="flex-grow">{hex}</div>
-        <button
-          onClick={handleCopyClick}
-          className="p-2 rounded hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-300"
-        >
-          <CopyIcon />
-        </button>
-      </div>
+
       <button
         className="mb-2 px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-300 transition-colors"
         type="button"
@@ -88,17 +114,27 @@ export function TextInscription() {
         <div className="text-blue-500">Waiting for transaction...</div>
       )}
       {isSuccess && (
-        <>
-          <div className="text-green-500">
-            Your inscription successfully created!{' '}
-            <a href={`https://etherscan.io/tx/${data?.hash}`}>
+        <div className="mt-4 p-4 bg-green-100 border border-green-400 rounded-md text-green-700">
+          <p>Your inscription was successfully created!</p>
+          <div className="flex flex-col sm:flex-row gap-2 mt-2">
+            <a
+              href={`https://etherscan.io/tx/${data?.hash}`}
+              className="text-blue-500 hover:text-blue-600 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               View on Etherscan
             </a>
-            <a href={`https://ethscriptions.com/ethscriptions/${data?.hash}`}>
+            <a
+              href={`https://ethscriptions.com/ethscriptions/${data?.hash}`}
+              className="text-blue-500 hover:text-blue-600 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               View on Ethscriptions
             </a>
           </div>
-        </>
+        </div>
       )}
       {isError && <div className="text-red-500">Error: {error?.message}</div>}
     </div>
