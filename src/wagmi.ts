239c9fd5667ae6628306be304895f0240dea26dd
Serge Kovbasiuk <isserge@protonmail.com>
2023-12-19 11:40:52 +0700

    bootstrap

---
+++ src/wagmi.ts
@@ -0,0 +1,30 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID
+
+console.log({ walletConnectProjectId })
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'EthScriptum',
+  chains,
+  projectId: walletConnectProjectId,
+})
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
